582cd66d618e02afbc121361a37a52689084438f
smunozc <salvador.munoz0109@gmail.com>
2020-11-11 13:40:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bserrang-ejercicio-3-avhhx9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/bserrang-ejercicio-3-avhhx9)
---
+++ script.js
@@ -0,0 +1,25 @@
+// En el siguiente ejercicio debéis sacar por consola (console.log) una lista con el nombre de los usuarios.
+// Dicha la lista la obtendréis del documento "users.json" haciendo uso de la función "fetch". Una vez tengáis el array de usuarios, debéis generar otro array con la función "filter" para que solo estén los usuarios que tengán más de 500 en la propiedad "money".
+// Teniendo la lista ya filtrada, la recorreréis con el método "forEach" para mostrar por consola el nombre de cada usuario.
+
+let buttonShowList = document.getElementById("showList");
+buttonShowList.addEventListener("click", showUsers);
+
+function getUsers() {
+  let array = [];
+  fetch("users.json")
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      array = data;
+    });
+}
+
+function getFilteredUsers(array) {
+  let array2 = array.filter(element => element.money > 500);
+}
+
+function showUsers() {
+  getUsers();
+  getFilteredUsers();
+}
